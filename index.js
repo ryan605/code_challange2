b2151f555af37d86d39b9185b928d5fdc8a5c71a
ryan605 <langatryan26@gmail.com>
2022-12-05 10:33:44 +0300

    complete code

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ function fetchData(){
     .then((json)=>console.log(json))
 }
 
+function createAnimalItem(thumbnail, name, id) {
+    // div to contain the elements
+  
+    let rootDiv = document.createElement("ol");
+    rootDiv.setAttribute("id", id);
+  
+    // animal image-the zoo
+    let animalImage = document.createElement("img");
+    animalImage.src = thumbnail;
+    animalImage.alt = `${name} image`;
+    animalImage.style.height = "150px";
+    animalImage.style.width = "200px";
+  
+    // animal name -the zoo
+    let animalTitle = document.createElement("h5");
+    animalTitle.innerHTML = name;
+  
+  
+  
+    // appending the image and text to root element
+    rootDiv.append(animalImage);
+    rootDiv.append(animalTitle);
+  
+  
+    // returns the element
+    return rootDiv;
+  }
+  
+ 
